dcca984e5886a25f2a0b3a0403cb5e0acdb01cd8
JOHNEDEL MAPA <mapajohnedel@gmail.com>
2022-07-01 09:59:00 +0800

    Update Webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+const { environment } = require('@rails/webpacker')
 
 module.exports = environment
